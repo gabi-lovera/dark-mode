887f5ca7ad504f4adb2859f3796ca5a6d3ab6375
Gabriel Lovera <gabrielloveraa@gmail.com>
2021-12-19 18:33:38 -0300

    provide global styles with context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./styles/themes.js";
+import Card from "./components/Card";
+import { CgSun } from "react-icons/cg";
+import { HiMoon } from "react-icons/hi";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  const text = theme === "light" ? "Dark Mode" : "Light Mode";
+  const icon = theme === "light" ? <CgSun size={40} /> : <HiMoon size={40} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <h1 className="head" onClick={() => themeToggler()}>
+          {text}&nbsp;
+          {icon}
+        </h1>
+        <Card themeToggler={themeToggler} />
+        <p className="made">Made by Gabriel</p>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
